828522bc389662c38c5e7b9d6280e1b0fef1ee40
Sabrine-Louss <loussaiefsabrine7@gmail.com>
2024-02-19 12:21:50 +0100

    adding files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Provider} from 'react-redux';
 import store from './redux/store';
 import axios from "axios";
 // axios.defaults.baseURL="http://localhost:5000/api";
-axios.defaults.baseURL="https://pet-care-api.onrender.com";
+axios.defaults.baseURL="https://pet-care-api.onrender.com/api";
 
 
 
